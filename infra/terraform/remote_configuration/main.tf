03a1d150cb716de57c80dc8023943029082704f9
aboulay <aboulay950@gmail.com>
2020-02-18 16:41:31 +0100

    Refactor name and add tags

--- infra/terraform/remote_configuration/main.tf
+++ infra/terraform/remote_configuration/main.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "configuration_bucket" {
-  bucket = "tezos-link-tfstate"
+resource "aws_s3_bucket" "tfstate_storage" {
+  bucket = "tzlink-tfstate"
 
   server_side_encryption_configuration {
     rule {
@@ -14,13 +14,16 @@ resource "aws_s3_bucket" "configuration_bucket" {
   }
 
   tags = {
-      Name = "tezos-link-tfstate"
-      Environment = "all"
+    Name        = "tzlink-tfstate"
+    Project     = "tezos-link"
+    Environment = "all"
+    BuildWith   = "terraform"
+    Trigramme   = "adbo"
   }
 }
 
-resource "aws_dynamodb_table" "configuration_dynamodb_table" {
-  name           = "tezos-link-tfstate-lock"
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name           = "tzlink-tfstate-lock"
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "LockID"
@@ -31,7 +34,10 @@ resource "aws_dynamodb_table" "configuration_dynamodb_table" {
   }
 
   tags = {
-    Name        = "tezos-link-tfstate-lock"
+    Name        = "tzlink-tfstate-lock"
+    Project     = "tezos-link"
     Environment = "all"
+    BuildWith   = "terraform"
+    Trigramme   = "adbo"
   }
 }
