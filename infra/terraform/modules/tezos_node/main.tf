50c4128b111fa8fd21dd26f757392567fd82eda7
aboulay <aboulay950@gmail.com>
2020-02-19 17:41:19 +0100

    Add module tezos_node in terraform (dev in progress) and use it with terragrunt

---
+++ infra/terraform/modules/tezos_node/main.tf
@@ -0,0 +1,79 @@
+data "aws_ami" "centos" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["679593333241"] # Canonical
+}
+
+data "aws_vpc" "tzlink" {
+  cidr_block = var.VPC_CIDR
+
+  tags = {
+      Name = format("tzlink-%s", var.ENV)
+      Environment = var.ENV
+  }
+}
+
+data "aws_subnet_ids" "tzlink" {
+  vpc_id = data.aws_vpc.tzlink.id
+
+  tags = {
+      Name = format("tzlink-%s-farm-*", var.ENV)
+  }
+}
+
+resource "aws_security_group" "tezos_node" {
+  name        = "tezos_node"
+  description = "Tezos node security group"
+  vpc_id      = data.aws_vpc.tzlink.id
+}
+
+resource "aws_security_group_rule" "ssh_ingress" {
+  type            = "ingress"
+  from_port       = 22
+  to_port         = 22
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.tezos_node.id
+}
+
+resource "aws_security_group_rule" "all_egress" {
+  type            = "egress"
+  from_port       = 0
+  to_port         = 0
+  protocol        = "-1"
+  cidr_blocks     = ["0.0.0.0/0"]
+  # prefix_list_ids = ["pl-12c4e678"] # TODO: useful for VPC endpoint. To check later.
+
+  security_group_id = aws_security_group.tezos_node.id
+}
+
+resource "aws_instance" "tz_node" {
+  ami           = data.aws_ami.centos.id
+  instance_type = "t2.micro"
+  subnet_id = tolist(data.aws_subnet_ids.tzlink.ids)[0]
+
+  key_name = "adbo"
+
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [ aws_security_group.tezos_node.id ]
+
+  tags = {
+    Name        = format("tzlink-%s-test", var.ENV)
+    Project     = "tezos-link"
+    Environment = var.ENV
+    BuildWith   = "terraform"
+    Trigramme   = "adbo"
+  }
+}
---
+++ infra/terragrunt/01_tezos_farm/terragrunt.hcl
@@ -0,0 +1,15 @@
+include {
+  path = "${find_in_parent_folders()}"
+}
+
+dependencies {
+  paths = ["../00_network"]
+}
+
+terraform {
+  source = "../../terraform/modules/tezos_node"
+}
+
+inputs = {
+  ENABLE_FLOW_LOGS = true
+}
