7fa6fa2cb5dd59e5fad9b9ca8a7be4d87fc6d5a9
aboulay <aboulay950@gmail.com>
2020-03-04 16:54:34 +0100

    Fix loadbalancer and tezos node security group

--- infra/terraform/modules/tezos_node/security_group.tf
+++ infra/terraform/modules/tezos_node/security_group.tf
@@ -9,7 +9,7 @@ resource "aws_security_group_rule" "rpc_ingress_for_tezos_node" {
   from_port   = 8000
   to_port     = 8000
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = [var.SUBNET_TZ_FARM_CIDR]
 
   security_group_id = aws_security_group.tezos_node.id
 }
@@ -30,7 +30,6 @@ resource "aws_security_group_rule" "all_egress_for_tezos_node" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-  # prefix_list_ids = ["pl-12c4e678"] # TODO: useful for VPC endpoint. To check later.
 
   security_group_id = aws_security_group.tezos_node.id
 }
@@ -46,7 +45,7 @@ resource "aws_security_group_rule" "http_ingress_for_loadbalancer" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = [var.VPC_CIDR]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.tezos_node_lb.id
 }
