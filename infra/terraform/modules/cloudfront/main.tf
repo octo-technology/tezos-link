d79da03964baac181085b439efa8c59adfdb8003
aboulay <aboulay950@gmail.com>
2020-03-10 18:33:45 +0100

    Fix cloudfront problem and all S3 to show object inside

--- infra/terraform/modules/cloudfront/main.tf
+++ infra/terraform/modules/cloudfront/main.tf
@@ -1,13 +1,33 @@
 resource "aws_s3_bucket" "tz_front" {
   bucket = "tezoslink-front"
   acl    = "public-read"
+  policy = <<EOF
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::tezoslink-front/*"]
+    }
+  ]
+}
+EOF
 
-  tags = {}
+  tags = {
+    Name        = "tezoslink-front"
+    Project     = var.PROJECT_NAME
+    BuildWith   = var.BUILD_WITH
+  }
 }
 
 resource "aws_cloudfront_distribution" "tz_front" {
   enabled             = true
   is_ipv6_enabled     = true
+
+  default_root_object = "index.html"
 
   origin {
     domain_name = aws_s3_bucket.tz_front.bucket_regional_domain_name
@@ -43,7 +63,10 @@ resource "aws_cloudfront_distribution" "tz_front" {
   aliases = ["tezoslink.io"]
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    #cloudfront_default_certificate = true
+    acm_certificate_arn = "arn:aws:acm:us-east-1:609827314188:certificate/05558bc2-3703-49ab-ae81-e22ea2ea437e"
+    ssl_support_method  = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
   }
 
   tags = {}
---
+++ infra/terraform/modules/cloudfront/variables.tf
@@ -0,0 +1,14 @@
+variable "REGION" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "PROJECT_NAME" {
+  type    = string
+  default = "tezos-link"
+}
+
+variable "BUILD_WITH" {
+  type    = string
+  default = "terraform"
+}
