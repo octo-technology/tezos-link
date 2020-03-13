dc9e7b69414b089267705e6764fbe46217238279
aboulay <aboulay950@gmail.com>
2020-03-13 21:16:16 +0100

    Remove deadcode

--- infra/terraform/modules/service_proxy/loadbalancer.tf
+++ infra/terraform/modules/service_proxy/loadbalancer.tf
@@ -39,21 +39,6 @@ resource "aws_alb_target_group" "proxy" {
   depends_on = [aws_alb.proxy]
 }
 
-#resource "aws_alb_listener" "proxy" {
-#  load_balancer_arn = aws_alb.proxy.id
-#  port              = 80
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    target_group_arn = aws_alb_target_group.proxy.id
-#    type             = "forward"
-#  }
-#
-#  depends_on = [aws_alb_target_group.proxy]
-#}
-
-############
-
 resource "aws_alb_listener" "proxy_https" {
   load_balancer_arn = aws_alb.proxy.arn
   port              = 443
