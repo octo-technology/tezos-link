d17d13ac606bd3dd65fb42d30dcd4dd907493a34
aboulay <aboulay950@gmail.com>
2020-03-16 13:21:55 +0100

    Apply certificates on the loadbalancer and add HTTPS rules in the security group for proxy

--- infra/terraform/modules/service_proxy/locals.tf
+++ infra/terraform/modules/service_proxy/locals.tf
@@ -5,6 +5,4 @@ locals {
   proxy_docker_image = "${var.PROXY_DOCKER_IMAGE_NAME}:${var.PROXY_DOCKER_IMAGE_VERSION}"
 
   ecs_task_logs_stream_prefix = format("tzlink-proxy-%s", var.TZ_NETWORK)
-
-  network_certificate_arn = "arn:aws:acm:us-east-1:609827314188:certificate/0e984566-3c5d-4c41-b211-461bb4c1b845"
 }
