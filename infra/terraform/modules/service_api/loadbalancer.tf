2587935c22a6f8b640b45659680b7f1494efb3ad
aboulay <aboulay950@gmail.com>
2020-03-16 13:22:06 +0100

    Apply certificates on the loadbalancer and add HTTPS rules in the security group for api

--- infra/terraform/modules/service_api/loadbalancer.tf
+++ infra/terraform/modules/service_api/loadbalancer.tf
@@ -39,12 +39,17 @@ resource "aws_alb_target_group" "api" {
   depends_on = [aws_alb.api]
 }
 
+data "aws_acm_certificate" "api" {
+  domain   = "api.tezoslink.io"
+  statuses = ["ISSUED"]
+}
+
 resource "aws_alb_listener" "api_https" {
   load_balancer_arn = aws_alb.api.arn
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-1-2017-01"
-  certificate_arn   = local.api_certificate_arn
+  certificate_arn   = data.aws_acm_certificate.api.arn
 
   default_action {
     target_group_arn = aws_alb_target_group.api.arn
