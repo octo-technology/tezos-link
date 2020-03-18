b9aea825e55c485100e59ba93d3b2f5775542015
aboulay <aboulay950@gmail.com>
2020-03-18 15:18:55 +0100

    Add private internal zone and enable_hostname in vpc

--- infra/terraform/modules/network/main.tf
+++ infra/terraform/modules/network/main.tf
@@ -1,6 +1,9 @@
 resource "aws_vpc" "tzlink" {
   cidr_block = var.VPC_CIDR
 
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name      = "tzlink"
     Project   = var.PROJECT_NAME
