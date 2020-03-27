c71cbd21bf563fd2cf9a99580e358f562169aebb
Loup Théron <ltheron@octo.com>
2020-03-27 17:26:33 +0100

    Fix status page by adding correct Project ID

--- web/src/pages/Status/Status.controller.tsx
+++ web/src/pages/Status/Status.controller.tsx
@@ -19,7 +19,7 @@ export const Status = () => {
 
     axios({
       method: 'get',
-      url: proxyUrl + '/v1/ece6cee2-09a3-4092-8cb2-a1c41602db86/chains/main/blocks/head'
+      url: proxyUrl + '/v1/9fc39568-f9c2-484c-a0a0-b3fbe62896de/chains/main/blocks/head'
     }).catch((error: any) => {
       console.error(error)
       setNodeStatus(false)
