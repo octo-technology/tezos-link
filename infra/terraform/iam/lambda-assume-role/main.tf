48fb8f6f0f63ae1ae295ac6fb6e45e170f3599a8
aboulay <aboulay950@gmail.com>
2020-06-22 12:26:18 +0200

    Refactor IAM policy for AWS lambda to handle multiple lambdas with different permission.

--- infra/terraform/iam/lambda-assume-role/main.tf
+++ infra/terraform/iam/lambda-assume-role/main.tf
@@ -12,7 +12,22 @@ data "aws_iam_policy_document" "tzlink_lambda_trusted_entity" {
   }
 }
 
-data "aws_iam_policy_document" "tzlink_lambda_access" {
+data "aws_iam_policy_document" "tzlink_global_lambda_access" {
+  statement {
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "tzlink_snapshot_lambda_access" {
   statement {
 
     actions = [
@@ -28,21 +43,35 @@ data "aws_iam_policy_document" "tzlink_lambda_access" {
   }
 
   statement {
-
     actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
+      "ec2:Describe*"
     ]
 
     resources = [
       "*"
     ]
   }
+}
+
+data "aws_iam_policy_document" "tzlink_metric_lambda_access" {
+  statement {
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      "arn:aws:s3:::tzlink-metric-lambda-dev",
+      "arn:aws:s3:::tzlink-metric-lambda-dev/*"
+    ]
+  }
 
   statement {
+
     actions = [
-      "ec2:Describe*"
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface"
     ]
 
     resources = [
