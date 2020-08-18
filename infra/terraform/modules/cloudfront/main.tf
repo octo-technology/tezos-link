ce9cb9fabaeb30463b4741254a349a677a79c2ab
Loup Théron <ltheron@octo.com>
2020-08-18 11:51:22 +0200

    Fix tf fmt

--- infra/terraform/modules/cloudfront/main.tf
+++ infra/terraform/modules/cloudfront/main.tf
@@ -76,7 +76,7 @@ resource "aws_cloudfront_distribution" "tz_front" {
       response_code         = "200"
       response_page_path    = "/index.html"
     }
-  ]  
+  ]
 
   default_cache_behavior {
     target_origin_id = "TZLinkFrontS3"
